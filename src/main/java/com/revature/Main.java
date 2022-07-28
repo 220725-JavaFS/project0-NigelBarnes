fdafd54e0ebd7618f1f09b331ddf8312904f0fde
Nigel Barnes <barnesnigel52@gmail.com>
2022-07-28 11:13:39 -0400

    Created some testing for the Scanner and User

--- src/main/java/com/revature/Main.java
+++ src/main/java/com/revature/Main.java
@@ -1,11 +1,28 @@
 package com.revature;
 
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main (String[]args) {
 		
-		System.out.println("Just checking");
+	Scanner gameUser = new Scanner(System.in);
+	
+	System.out.print("Please enter your username: ");
+	
+	String user = gameUser.nextLine();
+	
+	System.out.println("\nPerfect! Welcome back "+user+" it has been a long time since you've been here.\n");
+	
+	System.out.print("Now I need your password. Please enter it here: ");
+	
+	String pass = gameUser.nextLine();
+	
+	System.out.println("\nAre you sure your password is "+pass+"? I don't want to send the wrong information.");
 		
+	
+	gameUser.close();
+	
 		
   }
 
